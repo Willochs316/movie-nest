43ed1f7784b28c072f57928b87c6531162eb2a58
Willochs316 <willochs316@gmail.com>
2024-03-06 03:46:16 +0100

    refactor movie list component

--- components/MovieList/Category/PopularSeries.tsx
+++ components/MovieList/Category/PopularSeries.tsx
@@ -3,21 +3,21 @@ import { Svgs } from "@/app/assets/svgs";
 export default function PopularSeries({ popularSeries }: any): JSX.Element {
   return (
     <>
-      {popularSeries.map((movie: any, movieIndex: number) => (
+      {popularSeries.map((serie: any, movieIndex: number) => (
         <div key={movieIndex} className="card">
           <Svgs.Heart className="favorite" />
           <div className="movie-card__poster">
             <img
               className="p-0 m-0 w-fullh-full"
-              src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
-              alt={movie.title}
+              src={`https://image.tmdb.org/t/p/w500/${serie.poster_path}`}
+              alt={serie.title}
             />
           </div>
 
           <div className="movie-card__content">
-            <p>{movie.original_title}</p>
+            <p>{serie.original_name}</p>
             <p>
-              <span>{movie.release_date}</span>
+              <span>{serie.first_air_date}</span>
             </p>
           </div>
         </div>
